8752aad267baf44ab3f6f5bd23d69b240e8d53f2
SvetlAna00 <svetaktoto@gmail.com>
2021-08-18 18:58:49 +0400

    cart

---
+++ cart/productInfo.js
@@ -0,0 +1,56 @@
+let colorName = document.querySelector('.product_color');
+
+let pinkColor = '#fdded8';
+let pinkName = 'Pink';
+
+let changeColor = (colorSelect, colorNameSelect) => {
+    colorName.style.background = colorSelect.toString();
+    document.querySelector('.product_color').innerHTML = colorNameSelect;
+}
+
+changeColor(pinkColor, pinkName);
+
+// UNDO PRODUCT
+let removeProduct = document.querySelector('.remove_product');
+
+let undoProduct = () => {
+    product.style.display = "none";
+}
+
+removeProduct.addEventListener("click", undoProduct);
+
+// NUMBER OF PRODUCT
+
+let product = document.querySelector('.propduct_item')
+let number = document.querySelector('.number_product');
+
+counter = 1;
+number.innerHTML = counter;
+
+
+let reduceNumber = document.querySelector('.reduce_number_product');
+let addNumber = document.querySelector('.add_number_product');
+
+let updateNum = () => {
+    number.innerHTML = counter;
+}
+
+let lessNum = () => {
+    counter--;
+
+    updateNum();
+
+    if (counter <= 0) {
+        undoProduct();
+    }
+}
+
+let moreNum = () => {
+    counter++;
+    product.style.display = "";
+
+    updateNum();
+}
+
+reduceNumber.addEventListener("click", lessNum);
+addNumber.addEventListener("click", moreNum);
