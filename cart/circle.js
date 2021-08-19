983adedf310061c0afb52b9bfeb46ad2b3407c9d
SvetlAna00 <svetaktoto@gmail.com>
2021-08-19 14:08:52 +0400

    some edits

--- cart/circle.js
+++ cart/circle.js
@@ -1,21 +1,70 @@
 let circle = document.querySelector('.btn_circle');
-counterClicksCircle = 0;
-circle.style.left = "5px";
+promoCounterClicks = 0;
+pickupCounterClicks = 0;
+deliveryCounterClicks = 0;
 
-let changeBtn = () => {
-    counterClicksCircle++;
 
-    if (counterClicksCircle % 2 != 0) {
-        circle.style.background = "#F0B643";
-        circle.style.right = "5px";
-        circle.style.left = "";
+let allBtnCircle = document.querySelectorAll('.btn_circle');
+let activeBtnCircle = document.querySelectorAll('.btn_circle_active');
+
+let promoBtn = document.querySelector('#promo_btn');
+let pickupBtn = document.querySelector('#pickup_btn');
+let deliveryBtn = document.querySelector('#delivery_btn');
+
+let input = document.querySelector('.promo_input');
+
+let promoActive = (current) => {
+    promoCounterClicks++;
+    if (promoCounterClicks % 2 != 0) {
+        makeActive(current);
+        getPromo();
     }
     else {
-        circle.style.background = "";
-        circle.style.right = "";
-        circle.style.left = "5px";
+        disActive(current);
+        input.removeAttribute('readonly');
     }
-
 }
 
-circle.addEventListener("click", changeBtn);
+let getPromo = () => {
+    promoText = document.querySelector('.promo_input').value;
+    console.log(promoText);
+    input.readOnly = "true";
+}
+
+let pickupActive = (current) => {
+    pickupCounterClicks++;
+    if (pickupCounterClicks % 2 != 0 && !deliveryBtn.classList.contains('btn_circle_active')) {
+        makeActive(current);
+        document.querySelector('.pickup_select').style.display = "block";
+    }
+    else {
+        disActive(current);
+        document.querySelector('.pickup_select').style.display = "";
+    }
+}
+
+let deliveryActive = (current) => {
+    deliveryCounterClicks++;
+    if (deliveryCounterClicks % 2 != 0 && !pickupBtn.classList.contains('btn_circle_active')) {
+        makeActive(current);
+        document.querySelector('.select-delivery_section').style.display = "flex";
+    }
+    else {
+        disActive(current);
+        document.querySelector('.select-delivery_section').style.display = "";
+    }
+}
+
+let makeActive = (current) => {
+    current.target.classList.add('btn_circle_active');
+    current.target.classList.remove('btn_circle');
+}
+let disActive = (current) => {
+    current.target.classList.add('btn_circle');
+    current.target.classList.remove('btn_circle_active');
+}
+
+// circle.addEventListener("click", changeBtn);
+promoBtn.addEventListener("click", promoActive);
+pickupBtn.addEventListener("click", pickupActive);
+deliveryBtn.addEventListener("click", deliveryActive);
